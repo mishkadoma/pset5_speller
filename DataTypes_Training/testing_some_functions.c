33efea05f32943fdaf4b4e19cd2392fff727f52c
Mickhail <mixandoma@ya.ru>
2017-03-30 09:11:10 +0000

    wtf

---
+++ DataTypes_Training/testing_some_functions.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int x, int y);
+void pointers_swap(int *x, int *y);
+void malloc_swap(int x, int y);
+
+int main(void)
+{
+    int a = 10;
+    int b = 20;
+    
+    printf("Before swap: a = %d, b = %d\n", a, b);
+    
+    //trying swap without pointers
+    
+    swap(a, b);
+    printf("After swap: a = %d, b = %d\n", a, b);
+    
+    pointers_swap(&a, &b);
+    printf("After pointers_swap: a = %d, b = %d\n", a, b);
+    
+    malloc_swap(a, b);
+    printf("After malloc_swap: a = %d, b = %d\n", a, b);
+    return 0;
+}
+
+void swap(int x, int y)
+{
+    int tmp = x;
+    x = y;
+    y = tmp;
+    printf("swap...\n");
+}
+
+void pointers_swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+    printf("pointers_swap...\n");
+}
