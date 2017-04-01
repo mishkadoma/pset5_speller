2305cf508bd906633ba22db23c468dece76f9db5
Mickhail <mixandoma@ya.ru>
2017-04-01 07:28:16 +0000

    enter the value to find

--- DataTypes_Training/nodetest.c
+++ DataTypes_Training/nodetest.c
@@ -16,13 +16,27 @@ bool search(int n, node *list);
 
 int main(void)
 {
+    node1.n = 5;
+    node1.next = malloc(sizeof(node));
+    node1.next->n = 10;
+    node1.next->next = malloc(sizeof(node));
+    node1.next->next->n = 15;
     
-    node1.n = 4;
-    if (search(4, &node1))
-        printf("4 is in the list\n");
-    else
-        printf("Not in the list\n");
-    
+    while (true)
+    {
+        int user_input = get_int();
+        if (user_input < 0)
+        {
+            printf("Enter the positive number:");
+            return 5;
+        }
+        
+        
+        if (search(user_input, &node1))
+            printf("%d is in the list\n", user_input);
+        else
+            printf("Not in the list\n");
+    }    
 }
 
 bool search(int n, node *list)
